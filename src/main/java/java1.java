7298284e3d986c58831dd27c204f92357b53f589
AdemTEN <marrysnm2012@gmal.com>
2020-10-06 16:17:58 +0200

    first java projects

--- src/main/java/java1.java
+++ src/main/java/java1.java
@@ -2,6 +2,6 @@ public class java1 {
 
     public static void main(String[] args) {
         System.out.println("hi java");
-        System.out.println("\"hi java again\" = " + "hi java again");
+        System.out.println("\"hi java again\" = " + "hi java agai");
     }
 }
