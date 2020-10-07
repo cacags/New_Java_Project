048e7885a20fd979d7df6b27d9ef8f53e9339d61
Caglar Keskin <caglarkeskin@gmail.com>
2020-10-07 12:19:23 +0200

    new contribution

--- src/test/java/java2.java
+++ src/test/java/java2.java
@@ -1,5 +1,9 @@
 public class java2 {
     public static void main(String[] args) {
+
         System.out.println("hi java2");
+
+        //new contribution
+        System.out.println("Hello World. This is me.");
     }
 }
